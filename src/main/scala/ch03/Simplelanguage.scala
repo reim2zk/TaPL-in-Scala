262c02847f6a6a2fd19d60512c0e3f1b34b1fa04
curelemonade <c01150788f@edu.teu.ac.jp>
2019-07-09 01:40:34 +0900

    add Sの無限リスト

--- src/main/scala/ch03/Simplelanguage.scala
+++ src/main/scala/ch03/Simplelanguage.scala
@@ -29,6 +29,7 @@ final abstract class I_IfElse[T1 <: Term, T2 <: Term, T3 <: Term](t1: T1,
 //set
 object SetTheory {
   private type S = Set[Term]
+  private type StreamS = Stream[S]
 
   val S0: S = empty
 
@@ -47,18 +48,23 @@ object SetTheory {
       } yield expr2
     }
   }
+
+  val StreamS: Stream[S] = S0 #:: StreamS
+    .zip(SuccS(S0) #:: StreamS)
+    .map(s => SuccS(s._1))
+
 }
 
 //評価器
 object OneStepEval {
   val oneStepEval: Term => Term = {
-    case Succ(t) => Succ(oneStepEval(t))
-    case Pred(Zero) => Zero
-    case Pred(Succ(nv: NumericValue)) => nv
-    case Pred(t) => Pred(oneStepEval(t))
-    case IsZero(Zero) => True
+    case Succ(t)                       => Succ(oneStepEval(t))
+    case Pred(Zero)                    => Zero
+    case Pred(Succ(nv: NumericValue))  => nv
+    case Pred(t)                       => Pred(oneStepEval(t))
+    case IsZero(Zero)                  => True
     case IsZero(Succ(_: NumericValue)) => False
-    case IsZero(t) => IsZero(oneStepEval(t))
-    case _ => throw new RuntimeException
+    case IsZero(t)                     => IsZero(oneStepEval(t))
+    case _                             => throw new RuntimeException
   }
 }
