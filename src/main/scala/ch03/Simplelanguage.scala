8102210d577b37a363d32ff01d822db41dd19c01
YuMuuu <c01150788f@edu.teu.ac.jp>
2019-07-22 19:21:22 +0900

    wip

--- src/main/scala/ch03/Simplelanguage.scala
+++ src/main/scala/ch03/Simplelanguage.scala
@@ -3,21 +3,67 @@ package ch03
 import scala.collection.immutable.Set.empty
 
 //抽象構文木
-sealed trait Syntax
-sealed trait Term extends Syntax
-sealed trait Value extends Syntax
-sealed trait BadNat
-sealed trait BadBool
-sealed trait NumericValue extends Value with BadBool
+trait Syntax
+trait Term extends Syntax
+trait Value extends Syntax
+trait BadNat
+trait BadBool
+trait NumericValue extends Value with BadBool
+
+trait Zero extends Term with NumericValue with BadBool
+trait True[T <: Term] extends Term with Value with BadNat
+trait False[T <: Term] extends Term with Value with BadNat
+
+trait Succ[T <: Term] extends Term
+trait Pred[T <: Term] extends Term
+trait IsZero[T <: Term] extends Term
+trait IfElse[T1 <: Term, T2 <: Term, T3 <: Term] extends Term
+
 final case object Wrong extends BadBool with BadNat
 
-final case object True extends Term with Value with BadNat
-final case object False extends Term with Value with BadNat
-final case object Zero extends Term with NumericValue with BadBool
-final case class Succ(t: Term) extends Term
-final case class Pred(t: Term) extends Term
-final case class IsZero(t: Term) extends Term
-final case class IfElse(t1: Term, t2: Term, t3: Term) extends Term
+//final case object True extends Term with NumericValue with BadBool
+//final case object False extends Term with Value with BadNat
+//final case object Zero extends Term with Value with BadNat
+
+//final case class Succ(t: Term) extends Term
+//final case class Pred(t: Term) extends Term
+//final case class IsZero(t: Term) extends Term
+//final case class IfElse(t1: Term, t2: Term, t3: Term) extends Term
+
+trait ToInt[T <: Term] { def apply(): Int }
+object ToInt {
+  def apply[T <: Term](implicit toInt: ToInt[T]): Int = toInt()
+}
+
+trait ToBool[T <: Term] { def apply(): Boolean }
+object ToBool {
+  def apply[T <: Term](implicit toBool: ToBool[T]): Boolean = toBool()
+}
+
+object Calculater {
+  implicit def zero: ToInt[Zero] = () => 0
+
+  implicit def succ[T <: Term](implicit toInt: ToInt[T]): ToInt[Succ[T]] =
+    () => toInt() + 1
+
+  //TODO: 0未満にならないようにする必要がある？
+  implicit def pred[T <: Term](implicit toInt: ToInt[T]): ToInt[Pred[T]] =
+    () => toInt() + 1
+
+  implicit def isZero[T <: Term](implicit toInt: ToInt[T]): ToBool[IsZero[T]] =
+    () =>
+      toInt() match {
+        case 0 => true
+        case _ => false
+    }
+  implicit def ifElse[T1 <: Term, T2 <: Term, T3 <: Term](implicit toBool: ToBool[T1]): Term = {
+    () => toBool() match {
+      case true => T2
+      case false => T3
+    }
+  }
+
+}
 
 //set
 object SetTheory {
