84939286946dd6ecc5ee927b83dfae45c6b2c1b6
YuMuuu <c01150788f@edu.teu.ac.jp>
2019-07-30 04:14:02 +0900

    add info type and arg

--- src/main/scala/ch04/ML.scala
+++ src/main/scala/ch04/ML.scala
@@ -1,28 +1,29 @@
 package ch04
 
 sealed trait Term
-final case object TmTrue extends Term
-final case object TmFalse extends Term
-final case class TmIf(t1: Term, t2: Term, t3: Term) extends Term
-final case object TmZero extends Term
-final case class TmSucc(t1: Term) extends Term
-final case class TmPred(t1: Term) extends Term
-final case class TmIsZero(t1: Term) extends Term
+sealed trait Info
+final case class TmTrue(info: Info) extends Term
+final case class TmFalse(info: Info) extends Term
+final case class TmIf(info: Info, t1: Term, t2: Term, t3: Term) extends Term
+final case class TmZero(info: Info) extends Term
+final case class TmSucc(info: Info, t1: Term) extends Term
+final case class TmPred(info: Info, t1: Term) extends Term
+final case class TmIsZero(info: Info, t1: Term) extends Term
 
 class ML {}
 
 object isNumricVal {
   def apply(t: Term): Boolean = t match {
-    case TmZero           => true
-    case TmSucc(t1: Term) => isNumricVal(t1)
+    case TmZero(_)           => true
+    case TmSucc(_, t1: Term) => isNumricVal(t1)
     case _                => false
   }
 }
 
 object isVal {
   def apply(t: Term): Boolean = t match {
-    case TmTrue              => true
-    case TmFalse             => false
+    case TmTrue(_)              => true
+    case TmFalse(_)             => false
     case t if isNumricVal(t) => true
     case _                   => false
   }
