db69f8d978ad24771ba9072ceb7d4b803c661e54
YuMuuu <c01150788f@edu.teu.ac.jp>
2019-08-07 18:45:58 +0900

    wip

--- src/main/scala/ch04/ML.scala
+++ src/main/scala/ch04/ML.scala
@@ -55,10 +55,41 @@ object isVal {
         try {
           eval(evalOne(t))
         } catch {
-//          case e: Exception => throw e
           case _: NoRuleAppliesException => t
         }
       }
     }
   }
+}
+
+object bigStepEval {
+  def apply(t: Term): Term = t match {
+//    case t1 if isVal(t1) => t1
+//    case TmIf(_, t1, t2, t3)
+//      if isTrue(bigStepEval(t1)) && isVal(bigStepEval(t2)) =>
+//      bigStepEval(t2)
+//    case TmIf(_, t1, t2, t3)
+//      if isFalse(bigStepEval(t1)) && isVal(bigStepEval(t3)) =>
+//      bigStepEval(t3)
+//    case TmSucc(_, t1) if isNumericVal(bigStepEval(t1)) =>
+//      TmSucc(DummyInfo, bigStepEval(t1))
+//    case TmPred(_, t1) if isZero(bigStepEval(t1)) => TmZero(DummyInfo)
+//    case TmPred(_, TmSucc(_, nv)) if isNumericVal(bigStepEval(nv)) =>
+//      bigStepEval(nv)
+//    case TmIsZero(_, t1) if isZero(bigStepEval(t1)) => TmTrue(DummyInfo)
+//    case TmIsZero(_, _: TmSucc)                     => TmFalse(DummyInfo)
+//    case _                                          => t
+    case t1 => t1
+    case TmIf(_, TmTrue(_), t2, _) => bigStepEval(t2)
+    case TmIf(_, TmFalse(_), _, t3) => bigStepEval(t3)
+    case TmSucc(_, t1) if isNumricVal(t1) => bigStepEval(t1)
+    case TmPred(_, t1) if isZero(t1) => bigStepEval(t1)
+    case TmPred(_, t1) if isNumricVal(t1) => bigStepEval(t1)
+    case TmIsZero(fi, t1) if isZero(t1) => TmTrue(fi)
+    case TmIsZero(fi, t1) if !isZero(t1) => TmFalse(fi)
+
+  }
+
+ //TOOD: 実装する
+  def isZero(t: Term): Boolean = ???
 }
