7c4df9f1a01d5a0519658aa410d18c55a5bace86
curelemo <curelemonade15@gmail.com>
2019-08-12 19:12:16 +0900

    hot hot fixi

--- src/main/scala/ch04/ML.scala
+++ src/main/scala/ch04/ML.scala
@@ -27,29 +27,8 @@ object isVal {
   def apply(t: Term): Boolean = t match {
     case TmTrue(_)           => true
     case TmFalse(_)          => true
-    case t if isNumricVal(t) => true
+    case t if isNumericVal(t) => true
     case _                   => false
-  }
-}
-
-object isZero {
-  def apply(t: Term): Boolean = t match {
-    case TmZero(_) => true
-    case _         => false
-  }
-}
-
-object isTrue {
-  def apply(t: Term): Boolean = t match {
-    case TmTrue(_) => true
-    case _         => false
-  }
-}
-
-object isFalse {
-  def apply(t: Term): Boolean = t match {
-    case TmFalse(_) => true
-    case _          => false
   }
 }
 
@@ -60,10 +39,10 @@ object evalOne {
     case TmIf(fi, t1, t2, t3)                            => TmIf(fi, evalOne(t1), t2, t3)
     case TmSucc(fi, t1)                                  => TmSucc(fi, evalOne(t1))
     case TmPred(_, TmZero(_))                            => TmZero(DummyInfo)
-    case TmPred(_, TmSucc(_, nv1)) if isNumricVal(nv1)   => nv1
+    case TmPred(_, TmSucc(_, nv1)) if isNumericVal(t) => nv1
     case TmPred(fi, t1)                                  => TmPred(fi, evalOne(t1))
     case TmIsZero(_, TmZero(_))                          => TmTrue(DummyInfo)
-    case TmIsZero(_, TmSucc(_, nv1)) if isNumricVal(nv1) => TmFalse(DummyInfo)
+    case TmIsZero(_, TmSucc(_, nv1)) if isNumericVal(nv1) => TmFalse(DummyInfo)
     case TmIsZero(fi, t1)                                => TmIsZero(fi, evalOne(t1))
     case _                                               => throw new NoRuleAppliesException(t)
   }
@@ -78,23 +57,5 @@ object eval {
         case _: NoRuleAppliesException => t
       }
     }
-  }
-}
-
-object bigStepEval {
-  def apply(t: Term): Term = t match {
-    case t1 if isVal(t1) => t1
-    case TmIf(_, t1, t2, _)
-        if isTrue(bigStepEval(t1)) && isVal(bigStepEval(t2)) =>
-      bigStepEval(t2)
-    case TmIf(_, t1, _, t3)
-        if isFalse(bigStepEval(t1)) && isVal(bigStepEval(t3)) =>
-      bigStepEval(t3)
-    case TmSucc(_, t1) if isNumricVal(bigStepEval(t1)) => bigStepEval(t1)
-    case TmPred(_, t1) if isZero(bigStepEval(t1))      => bigStepEval(t1)
-    case TmPred(_, t1) if isNumricVal(bigStepEval(t1)) => bigStepEval(t1)
-    case TmIsZero(fi, t1) if isZero(bigStepEval(t1))   => TmTrue(fi)
-    case TmIsZero(fi, t1) if !isZero(bigStepEval(t1))  => TmFalse(fi)
-    case _                                             => t //TODO: 定義外
   }
 }
